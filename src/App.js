9c9afaaa47a00687107f75cf0899b711fdb4cfc6
pastaChaeng <123816211+pastaChaeng@users.noreply.github.com>
2023-06-30 15:02:07 +0800

    changes
    will update soon... laziness strike

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
+
   return (
-    <div className="App">
+    <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
+      <nav>
+        <div className="logo">
+          <img src="https://i.pinimg.com/564x/ad/78/5e/ad785e02b18f01b60cf792363cfa06bb.jpg" alt="Logo" />
+        </div>
+        <div className={`menu-icon ${menuOpen ? 'open' : ''}`} onClick={handleMenuToggle}>
+          <div className="menu-line"></div>
+          <div className="menu-line"></div>
+          <div className="menu-line"></div>
+        </div>
+        <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <li><a href="#Skills">Skills</a></li>
+          <li><a href="#Projects">Projects</a></li>
+          <li>
+            <label>
+              <input type="checkbox" checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+              <span className="dark-mode-icon">{darkMode ? '☾' : '☀'}</span> Dark Mode
+            </label>
+          </li>
+        </ul>
+      </nav>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bonjour!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Arlene Joy Nacion. I'm an aspiring shit specialized in frontend and backend development for complex scalable web apps.  
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section id="Skills" className="Skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Python</li>
+        </ul>
+      </section>
+      <section id="Projects" className="Projects">
+        <h2>Projects</h2>
+        <div className="Project-card">
+          <h3>Barangay Management System</h3>
+          <p>kunin ko muna docu</p>
+        </div>
+        <div className="Project-card">
+          <h3>Bratz Burger Cashier and Inventory System</h3>
+          <p>kunin ko muna docu</p>
+        </div>
+        <div className="Project-card">
+          <h3>Movie Ticket Reservation</h3>
+          <p>kunin ko muna docu</p>
+        </div>
+        <div className="Project-card">
+          <h3>Random Qoute Machine</h3>
+          <p>kunin ko muna docu</p>
+        </div>
+      </section>
+      <footer className="App-footer">
+        <p>
+          Thank you for visiting my portfolio. Feel free to <a href="mailto:your-arlenejoy.nacion777@gmail.com">get in touch</a>.
+        </p>
+      </footer>
     </div>
   );
 }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,56 +1,52 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
-export const navBar = () => {
-const  [activeLink, setActiveLink] = useState('home');
-const  [scolled, seScrolled] = useState(false);
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
 
-
-useEffect (() => {
+  useEffect(() => {
     const onScroll = () => { 
-        if (window.scrollY > 50) {
-            seScrolled(true);
-        } else{
-            seScrolled(false);
-         }
-        }
-
-        window.addEventListener("scroll", onScroll);
-
-        return () => window.removeEventListener("scroll", onScroll);
-
-    }, [])
-
-    const onUpdateActiveLink = (value) => {
-      setActiveLink(value);
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
 
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="Logo" />
+          <img src={'logo.png'} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-            </Navbar.Toggle>
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick ={() =>onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick ={() =>onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick ={() =>onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            </Nav>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
           <span className="navbar-text">
-              <div className="social-icon">
-              <a href="#"><img src={} alt="" /></a>
-              <a href="#"><img src={} alt="" /></a>
-               <a href="#"><img src={} alt="" /></a>
-
-              </div>
-              <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-
+            <div className="social-icon">
+              <a href="#"><img src={'icon1.png'} alt="" /></a>
+              <a href="#"><img src={'icon2.png'} alt="" /></a>
+              <a href="#"><img src={'icon3.png'} alt="" /></a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
-        </Navbar.Collapse>
+        </Navbar.Collapse>                       
       </Container>
     </Navbar>
   )
